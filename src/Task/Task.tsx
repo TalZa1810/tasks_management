c3bb5efbc5e057c8e899ec5ee5220f909a76a159
Tal Zaidman <tzaidman@newrelic.com>
2022-12-24 21:30:26 +0200

    adding grid to task component

--- src/Task/Task.tsx
+++ src/Task/Task.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { TasksPreviewList } from "../TasksPreviewList/TasksPreviewList";
-import { Tabs, TabList, TabPanels, Tab } from "@chakra-ui/react";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
 import { TaskDetails } from "../TaskDetails/TaskDetails";
 import { Status } from "../StatusBadge/StatusBadge";
 import { User } from "../User/User";
@@ -28,24 +35,42 @@ export const Task = ({
   relatedTickets,
 }: Task) => {
   return (
-    <>
-      <TaskHeader createdAt={createdAt} assignee={assignee} title={title} />
-      <TaskDetails
-        description={description}
-        assignee={assignee}
-        status={status}
-        createdAt={createdAt}
-      />
-      {relatedTickets && (
-        <Tabs size="md" variant="enclosed">
-          <TabList>
-            <Tab>Related Tasks</Tab>
-          </TabList>
-          <TabPanels>
-            <TasksPreviewList relatedTickets={relatedTickets} />
-          </TabPanels>
-        </Tabs>
-      )}
-    </>
+    <Grid
+      templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+      gridTemplateRows={"130px 1fr 30px"}
+      gridTemplateColumns={"150px 1fr"}
+      h="200px"
+      gap="1"
+    >
+      <GridItem pl="6" area="header">
+        <TaskHeader createdAt={createdAt} assignee={assignee} title={title} />
+      </GridItem>
+      <GridItem pl="2" area="nav">
+        Nav
+      </GridItem>
+      <GridItem pl="2" area="main">
+        <TaskDetails
+          description={description}
+          assignee={assignee}
+          status={status}
+          createdAt={createdAt}
+        />
+        {relatedTickets && (
+          <Tabs size="md" variant="enclosed">
+            <TabList>
+              <Tab>Related Tasks</Tab>
+            </TabList>
+            <TabPanels>
+              <TasksPreviewList relatedTickets={relatedTickets} />
+            </TabPanels>
+          </Tabs>
+        )}
+      </GridItem>
+      <GridItem pl="2" area={"footer"}>
+        Footer
+      </GridItem>
+    </Grid>
   );
 };
