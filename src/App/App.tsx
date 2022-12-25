f1640a4d8aa37e4934d26d464736cc207c24afdf
Tal Zaidman <tzaidman@newrelic.com>
2022-12-25 12:04:20 +0200

    improve structure

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,40 @@
 import * as React from "react";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  theme,
+  Grid,
+  GridItem,
+  Heading,
+} from "@chakra-ui/react";
 
 import { Task } from "../Task/Task";
 import { taskData } from "../data";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Task {...taskData} />
+    <Grid
+      templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+      gridTemplateRows={"130px 1fr 30px"}
+      gridTemplateColumns={"150px 1fr"}
+      h="200px"
+      gap="1"
+    >
+      <GridItem pl="6" area="header">
+        <Heading as="h1" size="3xl">
+          Task Management
+        </Heading>
+      </GridItem>
+      <GridItem pl="2" area="nav">
+        Nav
+      </GridItem>
+      <GridItem pl="2" area="main">
+        <Task {...taskData} />
+      </GridItem>
+      <GridItem pl="2" area={"footer"}>
+        Footer
+      </GridItem>
+    </Grid>
   </ChakraProvider>
 );
